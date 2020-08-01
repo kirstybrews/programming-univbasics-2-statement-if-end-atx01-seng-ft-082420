8069aa7c654c896341dff1e534ec719efaca8d38
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-01 18:09:20 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,7 +5,7 @@ if run_code_inside
 end
 puts "Code after if...end"
 
-chance_of_rain = 
+chance_of_rain = 1000
 if chance_of_rain <= 0.25
   puts "Pack a sun shelter!"
 elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
